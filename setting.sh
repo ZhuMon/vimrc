752dff8b6d9f52a1ac1354bdb1a3c7a66459a5e7
YuXiang <richardlin0212@gmail.com>
2019-06-01 22:03:47 +0800

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -10,4 +10,5 @@ rm ~/.vimrc
 link ./.vimrc ~/.vimrc
 mkdir ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 git clone https://github.com/scrooloose/nerdcommenter ~/.vim/bundle/nerdcommenter
