001ce2b0820aa0ce556a71669277d4b97ee8e953
ZhuMon <richardlin0212@gmail.com>
2020-02-27 16:53:12 +0800

    Fix some bugs

--- setEnv.sh
+++ setEnv.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-if [ "$(uname)"=="Darwin" ]; then
+sysOS=`uname`
+
+if [ $sysOS = "Darwin" ]; then
+    echo "On Mac OSX"
     echo "export CLANG_FORMAT_PATH to ~/.zshrc"
     echo "export CLANG_FORMAT_PATH=\"/usr/local/Cellar/clang-format/2019-05-14/share/clang/clang-format.py\"" >> ~/.zshrc
-elif [ "$(expr substr $(uname -s) 1 5)"=="Linux" ]; then
+
+elif [ $sysOS = "Linux" ]; then
+    echo "On Linux"
     echo "export CLANG_FORMAT_PATH to ~/.zshrc"
     echo "export CLANG_FORMAT_PATH=\"/usr/share/vim/addons/syntax/clang-format.py\"" >> ~/.zshrc
-elif [ "$(expr substr $(uname -s) 1 10)"=="MINGW32_NT" ]; then
+else
     echo "I don't know where clang-format.py is"
 fi
-
