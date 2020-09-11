90e2d9502e1bc86fa68ae5109aeec0daf3356327
ZhuMon <richardlin0212@gmail.com>
2020-09-11 18:07:45 +0800

    Update powerlevel9k to 10k

--- setzsh.sh
+++ setzsh.sh
@@ -2,21 +2,20 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-chsh -s /bin/zsh
+#chsh -s /bin/zsh
 
 sudo apt-get install powerline
 sudo apt-get install fonts-powerline
 
-git clone https://github.com/powerline/fonts.git ~/fonts
+#git clone https://github.com/powerline/fonts.git ~/fonts
 # install
-~/fonts/install.sh
+#~/fonts/install.sh
 
 
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
 git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
 
-exec zsh
+chsh -s /bin/zsh
