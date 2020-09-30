df41d9778bd22f5f20224018d994734205b730f6
ZhuMon <richardlin0212@gmail.com>
2020-09-30 15:34:33 +0800

    Add p10k config

--- setPowerlevel10k.sh
+++ setPowerlevel10k.sh
@@ -1,11 +1,6 @@
-echo "POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(virtualenv ssh context dir dir_writable vcs)" >> ~/.zshrc
-echo "POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status ram time) " >> ~/.zshrc
 echo "DEFAULT_USER=\"$USER\"" >> ~/.zshrc
-
-source ~/.zshrc
-
-#echo "set font to Ubunto Mono Regular"
 echo "ZSH_THEME=\"powerlevel10k/powerlevel10k\"" >> ~/.zshrc
 echo "plugins=(git\n\t\tzsh-syntax-highlighting\n\t\tzsh-autosuggestions)" >> ~/.zshrc
-echo "source \$ZSH/oh-my-zsh.sh"
-#echo "reboot"
+
+source ~/.zshrc
+p10k configure
