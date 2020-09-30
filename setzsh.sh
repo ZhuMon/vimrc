6a2d06161a7b2a0fd859a1049d6ac0c83cd8d7c7
ZhuMon <richardlin0212@gmail.com>
2020-09-30 15:29:14 +0800

    Add a comment

--- setzsh.sh
+++ setzsh.sh
@@ -2,7 +2,8 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-#chsh -s /bin/zsh
+
+# remember to exit zsh shell before finish install
 
 sudo apt-get install powerline
 sudo apt-get install fonts-powerline
