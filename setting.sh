d390ddcbcd820fa67d51793e27caaa743eb872be
ZhuMon <richardlin0212@gmail.com>
2021-05-22 14:31:31 +0800

    Install YouCompleteMe properly

--- setting.sh
+++ setting.sh
@@ -11,6 +11,7 @@
 sysOS=`uname`
 if [ $sysOS = "Darwin" ]; then
     echo "On Mac OSX"
+    brew install cmake python3 mono go nodejs
 
 elif [ $sysOS = "Linux" ]; then
     echo "On Linux, Install dependency"
@@ -18,6 +19,7 @@ elif [ $sysOS = "Linux" ]; then
     sudo apt-get install -y build-essential cmake python3-dev python3-pip zsh git curl sed clang-format
     # for vim plugin md-img-paste
     sudo apt-get install -y xclip
+    sudo apt install -y cmake vim-nox
     pip3 install autopep8
 elif [ `uname | cut -d"-" -f1` = "MINGW64_NT" ]; then
     pacman -S git mingw-w64-x86_64-gcc sed make
@@ -33,18 +35,15 @@ rm -f $HOME/.vimrc
 link ./.vimrc $HOME/.vimrc
 
 # plugin
-mkdir ~/.vim/bundle
-git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# temporally remove YouCompleteMe
-sed -i "s/Plugin 'Valloric\/YouCompleteMe'/\"@@@/" $HOME/.vimrc
-vim -E -s -u "$HOME/.vimrc" +PluginInstall +qall
-sed -i "s/\"@@@/Plugin 'Valloric\/YouCompleteMe'/" $HOME/.vimrc
+mkdir -p $HOME/.vim
+mkdir -p $HOME/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # install YouCompleteMe
-git clone --depth=1 https://github.com/Valloric/YouCompleteMe $HOME/.vim/bundle/YouCompleteMe
+git clone https://github.com/Valloric/YouCompleteMe $HOME/.vim/bundle/YouCompleteMe
 cd $HOME/.vim/bundle/YouCompleteMe
-git submodule update --init --recursive --depth=1
-python3 install.py --clang-completer
+git submodule update --init --recursive
+python3 install.py --clangd-completer
 
+vim -E -s -u "$HOME/.vimrc" +PluginInstall +qall
 cd ~
